9cf4536446239d76743ff965fbb51c1629bb4a9d
Christopher Hinstorff <chris@chinstorff.com>
2015-10-24 10:18:08 -0400

    add just_play, make more helper methods

--- api.py
+++ api.py
@@ -6,6 +6,24 @@ from base64 import b64encode
 SPOTIFY_CLIENT_ID = "29a0ec9178864f69b5e5e181811254ed"
 SPOTIFY_CLIENT_SECRET = "fc6b7b56e4154359a1b8569aece1301b"
 
+PLAYLISTS = [
+    {
+        'playlist_name': "Feel Good Indie Rock",
+        'playlist_id': "0XUlpafP8eIlIWt3VHSd7q",
+        'user_id': "spotify",
+    },
+    {
+        'playlist_name': "Have a Great Day!",
+        'playlist_id': "2PXdUld4Ueio2pHcB6sM8j",
+        'user_id': "spotify",
+    },
+    {
+        'playlist_name': "Teen Pop!",
+        'playlist_id': "445ES7sgFV8zJHebmbUW0L",
+        'user_id': "spotify",
+    },
+]
+
 app = Flask(__name__)
 
 
@@ -36,6 +54,36 @@ def call_spotify_api_get(url, access_token=None):
 
     return requests.get(url=url, headers=headers)
 
+def get_tracks(playlist_id, user_id):
+    url_base = "https://api.spotify.com/v1/users/%s/playlists/%s/tracks"
+
+    response = call_spotify_api_get(url_base % (user_id, playlist_id))
+
+    try:
+        tracks = response.json()['items']
+    except:
+        raise Exception("Spotify error: %s" % response.status_code)
+    
+    return tracks
+
+""" Run knapsack algorithm for list of tracks
+    
+    Parameters:
+      tracks: total pool of tracks to choose songs from
+
+    returns: serialized json of desired tracklist
+"""
+def knapsack_from_tracks(tracks, duration):
+    lengths = [item['track']['duration_ms'] / 1000 for item in tracks]
+
+    indices_to_play = knapsack(lengths, lengths, len(lengths), int(duration))
+    tracks_to_play = [tracks[i] for i in indices_to_play]
+
+    play_length = sum(lengths[i] for i in indices_to_play)
+    print "target:   %s\nachieved: %s\n" % (duration, play_length)
+
+    return jsonify(tracklist=tracks_to_play)
+    
 @app.route('/')
 def hello_world():
     return 'This is a!'
@@ -45,16 +93,9 @@ def tracks_for_duration():
     playlist_id = request.args.get('playlist_id')
     duration = request.args.get('duration')
 
-    url_base = "https://api.spotify.com/v1/users/%s/playlists/%s/tracks"
+    tracks = get_tracks_from_playlist_id(playlist_id)
 
-    tracks = call_spotify_api_get(url_base % ("spotify", playlist_id)).json()['items']
-    lengths = [item['track']['duration_ms'] / 1000 for item in tracks]
-
-    indices_to_play = knapsack(lengths, lengths, len(lengths), int(duration))
-    tracks_to_play = [tracks[i] for i in indices_to_play]
-
-    return jsonify(tracklist=tracks_to_play)
-
+    return knapsack_from_tracks(tracks, duration)
 
 @app.route('/update_tracklist')
 def update_tracklist():
@@ -62,9 +103,14 @@ def update_tracklist():
 
 @app.route('/just_play')
 def just_play():
-    pass
+    duration = request.args.get('duration')
 
+    tracks = []
+    for playlist in PLAYLISTS:
+        tracks.extend(get_tracks(playlist['playlist_id'], playlist['user_id']))
+    return knapsack_from_tracks(tracks, duration)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
