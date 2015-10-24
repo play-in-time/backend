26cf0644dc7465554106bca4c070ef65a5c76e87
Sam Slate <Sam@resnet142-64.medford.tufts.edu>
2015-10-24 01:49:46 -0400

    initialized flask file

---
+++ api.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, json
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'This is a!'
+
+@app.route('/tracks_for_duration')
+def tracks_for_duration():
+	#return flask.jason.jsonify()
+	pass
+
+@app.route('/update_tracklist')
+def update_tracklist():
+	pass
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
